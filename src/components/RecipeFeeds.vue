df25e8736c3b352b0884f3ec69b72f0acd703c8c
Grayfield <grayfieldmm@outlook.com>
2018-08-26 16:03:14 +0700

    layout done

---
+++ src/components/RecipeFeeds.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="recipe__feeds">
+     <div class="recipe__item">
+      <img src="" alt="Recipe Image">
+      <h5>Recipe Title</h5>
+      <p>Recipe Detail</p>
+      <p>Author</p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecipeFeeds"
+};
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -14,26 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('./views/Dashboard.vue')
     },
     {
-      path: '/dashboard',
-      component: () => import('./views/RecipeDashboard.vue'),
-      children: [
-        {
-          path: 'recipe/create',
-          component: () => import('./components/CreateRecipe.vue')
-        },
-        {
-          path: 'recipe/:id',
-          component: () => import('./components/RecipeDetail.vue')
-        }
-      ]
+      path: '/auth',
+      name: 'auth',
+      component: () => import('./views/Auth.vue')
     }
   ]
 })
